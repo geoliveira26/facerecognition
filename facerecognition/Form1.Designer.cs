fdcda14af8e47771c10573cbcb9fd3e816f17845
Gustavo Couto <gustavo.scouto@gmail.com>
2019-09-07 18:37:09 -0300

    Initial commit

---
+++ facerecognition/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace facerecognition
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imgCamUser = new Emgu.CV.UI.ImageBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imgCamUser)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imgCamUser
+            // 
+            this.imgCamUser.Location = new System.Drawing.Point(214, 79);
+            this.imgCamUser.Name = "imgCamUser";
+            this.imgCamUser.Size = new System.Drawing.Size(458, 303);
+            this.imgCamUser.TabIndex = 2;
+            this.imgCamUser.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.imgCamUser);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imgCamUser)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox imgCamUser;
+    }
+}
+
---
+++ facerecognition/RecognitionService.cs
@@ -0,0 +1,57 @@
+﻿using Emgu.CV.CvEnum;
+using Emgu.CV.Face;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace facerecognition
+{
+    public class RecognitionService
+    {
+        private FaceRecognizer _faceRecognizer;
+        private String _recognizerFilePath;
+
+        public RecognitionService(String databasePath, String recognizerFilePath)
+        {
+            _recognizerFilePath = recognizerFilePath;
+            _faceRecognizer = new EigenFaceRecognizer(80, double.PositiveInfinity);
+        }
+
+        public void TrainRecognizer()
+        {
+            var allFaces = _dataStoreAccess.CallFaces("ALL_USERS");
+            if (allFaces == null)
+                return;
+
+            var faceImages = new Image<Gray, byte>[allFaces.Count];
+            var faceLabels = new int[allFaces.Count];
+            for (int i = 0; i < allFaces.Count; i++)
+            {
+                var stream = new MemoryStream();
+                stream.Write(allFaces[i].Image, 0, allFaces[i].Image.Length);
+                var faceImage = new Image<Gray, byte>(new Bitmap(stream));
+                faceImages[i] = faceImage.Resize(100, 100, Inter.Cubic);
+                faceLabels[i] = allFaces[i].UserId;
+            }
+
+            _faceRecognizer.Train(faceImages, faceLabels);
+            _faceRecognizer.Write(_recognizerFilePath);
+        }
+
+        public void LoadRecognizerData()
+        {
+            _faceRecognizer.Read(_recognizerFilePath);
+        }
+
+        public int RecognizeUser(Image<Gray, byte> userImage)
+        {
+            /*  Stream stream = new MemoryStream();
+              stream.Write(userImage, 0, userImage.Length);
+              var faceImage = new Image<Gray, byte>(new Bitmap(stream));*/
+            _faceRecognizer.Read(_recognizerFilePath);
+
+            var result = _faceRecognizer.Predict(userImage.Resize(100, 100, Inter.Cubic));
+            return result.Label;
+        }
+    }
+}
